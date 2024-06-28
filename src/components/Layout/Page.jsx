826b4b89a60311a0f7f018e450c779658fd8f618
Ayush Jain <ayushjain2205@gmail.com>
2024-06-29 01:57:44 +0530

    Modify page min height

--- src/components/Layout/Page.jsx
+++ src/components/Layout/Page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Page = ({ children }) => {
   return (
-    <div className="relative flex flex-col items-center h-[665px] w-[390px] bg-black">
+    <div className="relative flex flex-col items-center min-h-[665px] w-[390px] bg-black">
       {children}
     </div>
   );
